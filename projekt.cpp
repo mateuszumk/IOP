51c4572b13bc2a620ea8ac95dd89bf050ab1df5d
Mateusz <blue_space_heroku@wp.pl>
2025-01-22 19:42:32 +0100

    dodano czy_pierwsza

--- projekt.cpp
+++ projekt.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
+bool czy_pierwsza(int liczba) {
+    if (liczba < 2) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int liczba;
     cin >> liczba;
     cout << "Podano liczbe: " << liczba << endl;
+    if (czy_pierwsza(liczba)) {
+        cout << liczba << " jest liczba pierwsza" << endl;
+    } else {
+        cout << liczba << " nie jest liczba pierwsza" << endl;
+    }
     return 0;
 }
 
